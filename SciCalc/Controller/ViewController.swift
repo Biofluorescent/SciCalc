ff606c95aec017ca3c1fed99e7e27edf722f38c8
tquesenberry <tanner.quesenberry@gmail.com>
2019-01-02 16:18:37 -0800

    Initial Commit

---
+++ SciCalc/Controller/ViewController.swift
@@ -0,0 +1,91 @@
+//
+//  ViewController.swift
+//  SciCalc
+//
+//  Created by Tanner Quesenberry on 12/13/18.
+//  Copyright © 2018 Tanner Quesenberry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sin: UIButton!
+    @IBOutlet weak var cos: UIButton!
+    @IBOutlet weak var tan: UIButton!
+    @IBOutlet weak var log: UIButton!
+    @IBOutlet weak var ln: UIButton!
+    @IBOutlet weak var yToX: UIButton!
+    @IBOutlet weak var displayNumber: UILabel!
+    
+    private var inverseActive : Bool = false
+    private var isFinishedTypingNumber : Bool = false
+    var memoryNumber : Double = 0
+    
+    private var displayValue : Double {
+        get {
+            guard let number = Double(displayNumber.text!) else {
+                fatalError("Cannot convert display label text to Double.")
+            }
+            return number
+        }
+        set {
+            displayNumber.text = String(newValue)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func inverseButtonPressed(_ sender: Any) {
+        if inverseActive == false {
+            sin.setTitle("sin⁻¹", for: .normal)
+            cos.setTitle("cos⁻¹", for: .normal)
+            tan.setTitle("tan⁻¹", for: .normal)
+            log.setTitle("10ˣ", for: .normal)
+            ln.setTitle("eˣ", for: .normal)
+            yToX.setTitle("ˣ√y", for: .normal)
+            inverseActive = true
+        }else {
+            sin.setTitle("sin", for: .normal)
+            cos.setTitle("cos", for: .normal)
+            tan.setTitle("tan", for: .normal)
+            log.setTitle("log", for: .normal)
+            ln.setTitle("ln", for: .normal)
+            yToX.setTitle("yˣ", for: .normal)
+            inverseActive = false
+        }
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+        displayValue = 0
+        isFinishedTypingNumber = true
+    }
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+//        print(sender.tag)
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayNumber.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                
+                if numValue == "." {
+                    
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt {
+                        return
+                    }
+                }
+                displayNumber.text = displayNumber.text! + numValue
+            }
+        }
+    }
+    
+    
+}
+
---
+++ SciCalc/Model/CalculatorLogic.swift
@@ -0,0 +1,15 @@
+//
+//  CalculatorLogic.swift
+//  SciCalc
+//
+//  Created by Tanner Quesenberry on 1/2/19.
+//  Copyright © 2019 Tanner Quesenberry. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    
+    
+}
